4f57287c012a3a64ac1e06fbb7a24aeece5cb76e
tijani <tijani-dia@github.com>
2021-05-23 12:18:55 +0200

    Setupt test suite

---
+++ testapp/liveblog/models.py
@@ -0,0 +1,8 @@
+from wagtail.core.models import Page
+from wagtail_live.models import LivePageMixin
+
+
+class SimpleLivePage(Page, LivePageMixin):
+    """A simple live page."""
+
+    content_panels = Page.content_panels + LivePageMixin.panels
