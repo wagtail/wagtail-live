46637a1a8a2b75b1a96929eff79e20676b048ea2
tijani <tijani-dia@github.com>
2021-06-02 16:20:15 +0200

    Models and receivers.

--- wagtail_live/models.py
+++ wagtail_live/models.py
@@ -59,7 +59,23 @@ class LivePageMixin(models.Model):
     ]
 
     def _get_live_post_index(self, live_post_id, low, high):
+        """ Retrieve index of a live post in page's live posts.
 
+        Uses binary search since live posts are sorted by timestamp.
+        This implementation may change.
+
+        Args:
+            live_post_id (str):
+                ID of the live post to look for.
+            low (int):
+                Index of first element in page live posts.
+            high (int):
+                Index of last element in page live_posts.
+
+        Returns:
+            (int) Index of the live post if found else -1
+        """
+        
         while low < high:
             mid = (low + high) // 2
             curr_id = self.live_posts[mid].id
@@ -72,31 +88,94 @@ class LivePageMixin(models.Model):
 
             else:
                 high = mid - 1
-
+        
+        if self.live_posts[low].id != live_post_id:
+            return -1
         return low
 
     def get_live_post_index(self, live_post_id):
-        """Returns an existing livepost with live_post_id."""
+        """Retrieve index of a livepost with its ID.
+        
+        Args:
+            live_post_id (str); Id of the live post to look for.
+
+        Returns:
+            (int) Index of the live post if found else -1
+        """
 
         return self._get_live_post_index(live_post_id, 0, len(self.live_posts))
 
     def get_live_post_by_index(self, live_post_index):
-        """Return a live post given its index in live_posts."""
+        """Retrieve a live post by its index.
+        
+        Args: 
+            live_post_index (str): Index of the live post to look for.
 
-        return self.live_posts[live_post_index]
+        Returns:
+            (LivePostBlock) The live post instance
+
+        Raises:
+            (someException) if a live post with the given index doesn't exist.
+        """
+
+        try:
+            live_post = self.live_posts[live_post_index]
+        except KeyError:
+            raise 
+        return live_post
 
     def get_live_post_by_id(self, live_post_id):
+        """Retrieve a live post by its ID.
+        
+        Args: 
+            live_post_id (str): ID of the live post to look for.
+
+        Returns:
+            (LivePostBlock) The live post instance
+
+        Raises:
+            (someException) if a live post with the given ID doesn't exist.
+        """
+
         live_post_index = self.get_live_post_index(live_post_id=live_post_id)
+        if live_post_index == -1:
+            raise
         return self.get_live_post_by_index(live_post_index)
 
     def add_block_to_live_post(self, block_type, block, live_block):
+        """ Add a new content block to a live post.
+        
+        Args:
+            block_type (str):
+                Type of the block to add
+            block (Block):
+                Block to add to the live post.
+            live_block (LivePostBlock):
+                Live post in which the new block will be added.
+        """
+
         live_block["content"].append((block_type, block))
 
     def add_live_post(self, live_post, live_post_id):
+        """ Add a new live post to live page.
+
+        Live posts are added with a custom ID to facilitate
+        keeping their track. 
+        By default, the value of a live post's ID is
+        the timestamp of the corresponding message creation. 
+        A check is also done to ensure that live posts are always
+        sorted by time.
+
+        Args:
+            live_post (LivePostBlock):
+                live post to add
+            live_post_id (str):
+                Id of the new live_post
+        """
+
         lp_index = len(self.live_posts) if self.live_posts else 0
-        while self.live_posts and float(self.live_posts[lp_index - 1].id) > float(
-            live_post_id
-        ):
+        post_id = float(live_post_id)
+        while self.live_posts and float(self.live_posts[lp_index - 1].id) > post_id:
             lp_index -= 1
 
         # Insert to keep posts sorted by time
@@ -106,15 +185,38 @@ class LivePageMixin(models.Model):
         self.save()
 
     def delete_live_post(self, live_post_id):
+        """Delete a live post by its ID.
+        
+        Args:
+            live_post_id (str): Id of the live post to delete.
+
+        Raises:
+            (someException) if live post with the given ID doesn't exist.
+        """
+
         live_post_index = self.get_live_post_index(live_post_id=live_post_id)
+        if live_post_index == -1:
+            raise
         del self.live_posts[live_post_index]
         self.last_update_at = now()
         self.save()
 
     def clear_live_post_content(self, live_post):
+        """Clears the content of a live post.
+
+        Args:
+            live_post (livePostBlock): Live post which content will be cleared.
+        """
+    
         live_post.value.get("content").clear()
 
     def update_live_post(self, live_post):
+        """Update a live post when it has been edited.
+
+        Args:
+            live_post (livePostBlock): Live post to update.
+        """
+    
         live_post.value["modified"] = now()
         self.last_update_at = now()
         self.save()
---
+++ wagtail_live/receivers.py
@@ -0,0 +1,280 @@
+"""Wagtail Live receiver classes."""
+
+from wagtail.embeds.oembed_providers import all_providers
+
+from .blocks import (
+    construct_embed_block,
+    construct_image_block,
+    construct_live_post_block,
+    construct_text_block,
+)
+
+TEXT = "message"
+IMAGE = "image"
+EMBED = "embed"
+LivePost = "live_post"
+
+
+def is_embed(text):
+    for provider in all_providers:
+        for url_pattern in provider.get("urls", []):
+            # Somehow Slack links start with `<` and end with `>`.
+            if bool(re.match(url_pattern, text)):
+                return True
+
+    return False
+
+
+class BaseMessageReceiver:
+    """Base Receiver class."""
+
+    def __init__(self, app_name, model_name):
+        """ 
+        LivePageMixin is an abstract class, so we can't make queries directly
+        We have to get the actual page which subclasses it to perform queries.
+        """
+
+        self.model = apps.get_model(app_name, model_name)
+
+    def dispatch(self, event):
+        """ Dispatch an event to find corresponding handler.
+
+        Args:
+            event: New event from a messaging app.
+        """
+
+        raise NotImplementedError
+
+    def get_channel_name_from_message(self, message):
+        """ Retrieve a channel name from a message.
+
+        Args:
+            message: A received message from a messaging app
+
+        Returns:
+            Name of the channel which the given message belongs to
+        """
+
+        raise NotImplementedError
+
+    def get_live_page_from_channel_name(self, channel_name):
+        """ Retrieve the live page with a given channel name.
+
+        Args:
+            channel_name (str): Channel name
+
+        Returns:
+            (LivePageMixin) The livepage corresponding to channel name.
+
+        Raises:
+            model.DoesNotExist if a page with the given channel name doesn't exist.
+        """
+
+        try:
+            live_page = self.model.objects.get(channel_name=channel_name)
+            return live_page
+        except self.model.DoesNotExist:
+            raise
+
+    def get_message_id_from_message(self, message):
+        """ Retrieve message's ID.
+
+        Args:
+            message: A received message from a messaging app
+
+        Returns:
+            Id of the given message
+        """
+        
+        raise NotImplementedError
+
+    def get_message_text(self, message):
+        """Retrieve the text of a message.
+
+        A message is made of text and files.
+
+        Args: 
+            message: A received message from a messaging app
+
+        Returns:
+            (str) Text of the given message.
+        """
+
+        raise NotImplementedError
+
+    def get_message_files(self, message):
+        """Retrieve the files of a message.
+
+        A message is made of text and files.
+
+        Args: 
+            message: A received message from a messaging app
+
+        Returns:
+            (list) of files included in the given message.
+        """
+
+        raise NotImplementedError
+
+    def get_message_id_from_edited_message(self, message):
+        """ Retrieve the ID of the original message.
+
+        Args: 
+            message: A received message from a messaging app
+
+        Returns:
+            ID of the original message thet is being edited.
+        """
+
+        raise NotImplementedError
+
+    def get_message_text_from_edited_message(self, message):
+        """ Retrieve the text an edited message
+
+        Args: 
+            message: A received message from a messaging app
+
+        Returns:
+            Text of the edited message
+        """
+
+        raise NotImplementedError
+
+    def get_message_files_from_edited_message(self, message):
+        """ Retrieve the files  from an edited message
+
+        Args: 
+            message: A received message from a messaging app
+
+        Returns:
+            files of the edited message
+        """
+
+        raise NotImplementedError
+
+    def process_text(self, live_page, live_post, message_text):
+        """ Processes the text of a message.
+
+        Parses the message, constructs corresponding block types
+        i.e Embed or Text and add those blocks to the given
+        live post.
+
+        Args:
+            live_page (LivePageMixin):
+                Live page to update
+            live_post (LivePostBlock):
+                Live post to update
+            message_text (str):
+                Text to add to a live post.
+        """
+
+        message_parts = message_text.split("\n")
+        for part in message_parts:
+            block_type = ""
+
+            if is_embed(part[1:-1]):
+                # Strip leading `<` and trailing `>`.
+                # Not sure if it's the normal behavior, but have repeatedly received links
+                # from SLack API that looks like below:
+                # <https://twitter.com/lephoceen/status/139?s=20|https://twitter.com/lephoceen/status/139?s=20>'
+
+                url = part[1:-1].split("|")[0]
+                block = construct_embed_block(url)
+                block_type = EMBED
+
+            else:
+                block = construct_text_block(part)
+                block_type = TEXT
+
+            live_page.add_block_to_live_post(block_type, block, live_post)
+
+    def process_files(self, live_page, live_post, files):
+        """ Processes the files of a message.
+
+        Creates the corresponding block for any file and add it
+        to the given live post.
+
+        Args:
+            live_page (LivePageMixin):
+                Live page to update
+            live_post (LivePostBlock):
+                Live post to update
+            files (list):
+                Files to add to a live post.
+        """
+
+        for item in files:
+            mime_type = item["mimetype"]
+            if mime_type in ["image/png", "image/jpeg", "image/gif"]:
+
+                filename = item.get("name")
+                file_url = item["url_private"]
+                headers = {"Authorization": f"Bearer {settings.SLACK_BOT_TOKEN}"}
+                response = requests.get(file_url, headers=headers)
+
+                img = get_image_model()(
+                    title=slugify(filename).replace("-", " "),
+                    file=ContentFile(
+                        response.content,
+                        name=filename,
+                    ),
+                )
+                img.save()
+                block = construct_image_block(img)
+                live_page.add_block_to_live_post(IMAGE, block, live_post)
+
+    def add_message(self, message):
+        """ Add a received message from a messaging app to the corresponding channel.
+
+        Args:
+            message: A message received from a messaging app
+        """
+        
+        message_id = self.get_message_id_from_message(message)
+        channel_name = self.get_channel_name_from_message(message)
+        live_page = self.get_live_page_from_channel_name(channel_name)
+
+        live_post = construct_live_post_block(message_id, now())
+
+        message_text = self.get_message_text(message)
+        self.process_text(live_page, live_post, message_text)
+
+        files = self.get_message_files(message)
+        self.process_files(live_page, live_post, files)
+
+        live_page.add_live_post(live_post, message_id)
+
+    def change_message(self, message):
+        """ Change a message when it's edited from a messaging app.
+
+        Args:
+            message: A message edited from a messaging app
+        """
+
+        message_id = self.get_message_id_from_edited_message(message)
+        channel_name = self.get_channel_name_from_message(message)
+        live_page = self.get_live_page_from_channel_name(channel_name)
+
+        live_post = live_page.get_live_post_by_id(live_post_id=message_id)
+        live_page.clear_live_post_content(live_post)
+
+        message_text = self.get_message_text_from_edited_message(message)
+        self.process_text(live_page, live_post.value, message_text)
+
+        files = self.get_message_files_from_edited_message(message)
+        self.process_files(live_page, live_post.value, files)
+
+        live_page.update_live_post(live_post)
+
+    def delete_message(self, message):
+        """ Delete a message when it's deleted from a messaging app.
+
+        Args:
+            message: A message deleted from a messaging app
+        """
+
+        message_id = self.get_message_id_from_edited_message(message)
+        channel_name = self.get_channel_name_from_message(message)
+        live_page = self.get_live_page_from_channel_name(channel_name)
+
+        live_page.delete_live_post(message_id)
