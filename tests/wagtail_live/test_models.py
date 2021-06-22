0a44cb3058f4164c79469b8a36fd197ad8921e7e
tijani <tijani-dia@github.com>
2021-06-22 16:38:53 +0200

    Rename life_posts to live_posts

--- tests/wagtail_live/test_models.py
+++ tests/wagtail_live/test_models.py
@@ -5,7 +5,7 @@ import pytest
 
 @pytest.mark.django_db
 def test_blog_page_update_live_posts_creates_revision(blog_page_factory):
-    life_posts = json.dumps(
+    live_posts = json.dumps(
         [
             {
                 "type": "live_post",
@@ -20,7 +20,7 @@ def test_blog_page_update_live_posts_creates_revision(blog_page_factory):
             }
         ]
     )
-    page = blog_page_factory(channel_id="some-id", live_posts=life_posts)
+    page = blog_page_factory(channel_id="some-id", live_posts=live_posts)
     assert page.revisions.count() == 0
     page.delete_live_post("743fbd4d-929a-4b85-8f30-93cb19d6a933")
     assert page.revisions.count() == 1
