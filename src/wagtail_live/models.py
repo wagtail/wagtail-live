d9fb58ca1d631e9fa99f103313c7d37699caffb6
tijani <tijani-dia@github.com>
2021-06-22 11:12:06 +0200

    Make channel_id optional

--- src/wagtail_live/models.py
+++ src/wagtail_live/models.py
@@ -34,6 +34,8 @@ class LivePageMixin(models.Model):
     channel_id = models.CharField(
         help_text="Channel ID",
         max_length=255,
+        null=True,
+        blank=True,
     )
 
     last_update_at = models.DateTimeField(
