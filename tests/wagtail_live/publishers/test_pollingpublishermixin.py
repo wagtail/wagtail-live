51c660c236c6386ced04a1cc4f5a16e2df3c726b
tijani <tijani-dia@github.com>
2021-06-28 11:44:02 +0200

    Added polling publishers tests

---
+++ tests/wagtail_live/publishers/test_pollingpublishermixin.py
@@ -0,0 +1,50 @@
+import pytest
+from django.urls.resolvers import URLPattern
+from django.views import View
+
+from tests.testapp.models import BlogPage
+from wagtail_live.publishers import PollingPublisherMixin
+
+
+@pytest.fixture
+def polling_mixin():
+    return PollingPublisherMixin()
+
+
+def test_polling_mixin_instance(polling_mixin):
+    assert isinstance(polling_mixin, View)
+
+
+def test_polling_mixin_model(polling_mixin):
+    assert polling_mixin.model == BlogPage
+
+
+def test_get_urls():
+    patterns = PollingPublisherMixin.get_urls()
+    assert len(patterns) == 1
+
+    pattern = patterns[0]
+    assert isinstance(pattern, URLPattern)
+    assert pattern.pattern._route == "get-updates/<str:channel_id>/"
+    assert pattern.callback.view_class == PollingPublisherMixin
+    assert pattern.name == ""
+
+
+def test_get_last_update_client_from_request(polling_mixin, rf):
+    request = rf.get("/", {"last_update_ts": "1000.00"})
+
+    assert polling_mixin.get_last_update_client_from_request(request=request) == 1000.00
+
+
+def test_post_raise_not_implemented_error(polling_mixin, rf):
+    request = rf.post("/")
+
+    with pytest.raises(NotImplementedError):
+        polling_mixin.post(request=request, channel_id=1)
+
+
+def test_get_raise_not_implemented_error(polling_mixin, rf):
+    request = rf.get("/")
+
+    with pytest.raises(NotImplementedError):
+        polling_mixin.get(request=request, channel_id=1)
