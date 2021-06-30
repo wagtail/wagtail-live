69d6446bc1947b893f3aca1ee49a0064b356b6a2
Storm Heg <storm@stormbase.digital>
2021-06-30 15:56:39 +0200

    Test with Django 2.2 (#54)
    * Add Django 2.2 to test suite
    
    * Update github actions workflow with new tox envs

--- tests/settings.py
+++ tests/settings.py
@@ -55,7 +55,7 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3"),
     }
 }
 
