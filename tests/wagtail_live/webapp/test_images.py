e91c94ac0f151e773762467aa8500c8baba2894d
tijani <tijani-dia@github.com>
2021-07-08 00:18:55 +0200

    Add test for Image model

---
+++ tests/wagtail_live/webapp/test_images.py
@@ -0,0 +1,29 @@
+""" Webapp Image test suite """
+
+from django.test import TestCase
+
+from tests.utils import get_test_image_file
+from wagtail_live.webapp.models import Channel, Image, Message
+
+
+class ImageAPITests(TestCase):
+    def setUp(self):
+        channel = Channel.objects.create(channel_name="test_channel")
+        message = Message.objects.create(channel=channel, content="Some content")
+
+        image_content = get_test_image_file()
+        self.image = Image.objects.create(message=message, image=image_content)
+
+    def test_delete_image(self):
+        self.assertEqual(Image.objects.count(), 1)
+        response = self.client.delete(f"/webapp/api/images/{self.image.id}/")
+
+        self.assertEqual(response.status_code, 204)
+        self.assertEqual(Image.objects.count(), 0)
+
+    def test_delete_image_wrong_pk(self):
+        self.assertEqual(Image.objects.count(), 1)
+        response = self.client.delete("/webapp/api/images/2/")
+
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(Image.objects.count(), 1)
