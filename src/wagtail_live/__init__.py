c28b6bdc9ae8edec93a6ab6a6996567b9ff5315f
Storm Heg <storm@stormbase.digital>
2021-07-07 23:12:28 +0200

    Back to development: alpha 3

--- src/wagtail_live/__init__.py
+++ src/wagtail_live/__init__.py
@@ -2,6 +2,6 @@ from .version import get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, "alpha", 2)
+VERSION = (1, 0, 0, "alpha", 3)
 
 __version__ = get_version(VERSION)
