46f5f957c147b4fafc8ae81e71e07575d29990ab
Tidiane <atdia97@gmail.com>
2021-07-25 21:45:32 +0200

    Use AsyncJsonWesocketConsumer2

--- src/wagtail_live/publishers/django_channels/app.py
+++ src/wagtail_live/publishers/django_channels/app.py
@@ -1,31 +1,30 @@
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import JsonWebsocketConsumer
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
 from django.urls import re_path
 
 
-class DjangoChannelsApp(JsonWebsocketConsumer):
+class DjangoChannelsApp(AsyncJsonWebsocketConsumer):
     """App/Consumer which handles websocket connections."""
 
-    def connect(self):
+    async def connect(self):
         """Adds websocket channel to room group."""
 
         self.channel_id = self.scope["url_route"]["kwargs"]["channel_id"]
         self.group_name = f"liveblog_{self.channel_id}"
 
-        async_to_sync(self.channel_layer.group_add)(self.group_name, self.channel_name)
-        self.accept()
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         """Discards websocket channel from room group."""
 
-        async_to_sync(self.channel_layer.group_discard)(
-            self.group_name, self.channel_name
-        )
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
-    def new_update(self, event):
+    async def new_update(self, event):
         """Receives messages from room group and sends them to websocket client."""
 
-        self.send_json({"renders": event["renders"], "removals": event["removals"]})
+        await self.send_json(
+            {"renders": event["renders"], "removals": event["removals"]}
+        )
 
 
 live_websocket_route = [
