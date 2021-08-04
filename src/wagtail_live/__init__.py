cd8af9b8104a28c44075b9589d4b7ed8e18b62df
Tidiane <atdia97@gmail.com>
2021-08-04 11:32:02 +0200

    Back to alpha 4

--- src/wagtail_live/__init__.py
+++ src/wagtail_live/__init__.py
@@ -2,7 +2,7 @@ from .version import get_version
 
 # major.minor.patch.release.number
 # release must be one of alpha, beta, rc, or final
-VERSION = (1, 0, 0, "alpha", 3)
+VERSION = (1, 0, 0, "alpha", 4)
 
 __version__ = get_version(VERSION)
 
