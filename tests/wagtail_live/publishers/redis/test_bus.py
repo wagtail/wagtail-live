5e99ce34ee051d9776af44135a0b34ac0e4a3eb1
Tidiane <atdia97@gmail.com>
2021-08-04 15:10:47 +0200

    Add AsyncMock

--- tests/wagtail_live/publishers/redis/test_bus.py
+++ tests/wagtail_live/publishers/redis/test_bus.py
@@ -8,21 +8,25 @@ from wagtail_live.publishers.utils import get_redis_url
 from ..conftest import wait_for_message
 
 
-async def broadcast():
+async def broadcast(message, recipients):
     pass
 
 
 @pytest.fixture
-def bus():
-    return RedisBus(get_redis_url(), broadcast)
+def mocked(mocker):
+    return mocker.AsyncMock(broadcast, return_value=None)
+
+
+@pytest.fixture
+def bus(mocked):
+    return RedisBus(get_redis_url(), mocked)
 
 
 @pytest.mark.asyncio
-async def test_pubsub(bus, mocker, redis):
+async def test_pubsub(bus, mocked, redis):
     channel_group_name = make_channel_group_name("test_channel")
     other_channel_group_name = make_channel_group_name("other_channel")
     ws_1, ws_2, ws_3 = "ws_1", "ws_2", "ws_3"
-    mocker.patch.object(bus, "broadcast")
 
     await bus.subscribe(channel_group_name, ws_1)
     await bus.subscribe(channel_group_name, ws_2)
@@ -39,7 +43,7 @@ async def test_pubsub(bus, mocker, redis):
     # it is broadcasted to websocket connections that have subscribed to that channel group.
     await redis.publish(channel_group_name, "hey")
     await wait_for_message(bus.pubsub)
-    bus.broadcast.assert_called_once_with("hey", {ws_1, ws_2})
+    mocked.assert_called_once_with("hey", {ws_1, ws_2})
 
     await bus.unsubscribe(other_channel_group_name, ws_3)
     assert bus.get_channel_group_subscribers(other_channel_group_name) == set()
