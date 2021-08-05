c40e688ec5d716686d4b1ede5ae55bc7b722891d
Tidiane <atdia97@gmail.com>
2021-08-05 08:49:48 +0200

    Add publisher test

---
+++ tests/wagtail_live/publishers/piesocket/test_publisher.py
@@ -0,0 +1,54 @@
+import json
+
+import requests
+from django.test import override_settings
+
+from wagtail_live.publishers.piesocket.publisher import (
+    PieSocketPublisher,
+    headers,
+    publish_url,
+)
+
+
+class ResponseMock:
+    def __init__(self, ok):
+        self.ok = ok
+
+        def ok(self):
+            return self.ok
+
+
+API_KEY = "api-key"
+SECRET = "not-secret"
+
+
+@override_settings(PIESOCKET_API_KEY=API_KEY, PIESOCKET_SECRET=SECRET)
+def test_publisher(mocker):
+    publisher = PieSocketPublisher()
+    channel_id = "some-id"
+    renders = removals = []
+    mocker.patch.object(requests, "post", return_value=ResponseMock(ok=True))
+
+    publisher.publish(channel_id, renders, removals)
+
+    expected = json.dumps(
+        {
+            "key": API_KEY,
+            "secret": SECRET,
+            "channelId": channel_id,
+            "message": {"renders": renders, "removals": removals},
+        }
+    )
+    requests.post.assert_called_once_with(publish_url, headers=headers, data=expected)
+
+
+@override_settings(PIESOCKET_API_KEY=API_KEY, PIESOCKET_SECRET=SECRET)
+def test_publisher_response_not_ok(mocker, caplog):
+    publisher = PieSocketPublisher()
+    channel_id = "some-id"
+    renders = removals = []
+    mocker.patch.object(requests, "post", return_value=ResponseMock(ok=False))
+
+    publisher.publish(channel_id, renders, removals)
+
+    assert caplog.messages[0] == "Failed publishing new update to PieSocket API."
