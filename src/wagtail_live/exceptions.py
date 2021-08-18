d0790c15cb30168adcf4efc342a59febedaa7830
Tidiane <atdia97@gmail.com>
2021-08-18 12:22:59 +0200

    Add excpetions docstrings

--- src/wagtail_live/exceptions.py
+++ src/wagtail_live/exceptions.py
@@ -2,8 +2,12 @@
 
 
 class RequestVerificationError(Exception):
+    """A incoming request couldn't be verified."""
+
     pass
 
 
 class WebhookSetupError(Exception):
+    """An error occured while setting a webhook connection with the input source."""
+
     pass
