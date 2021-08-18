3e366ab558df703f4bf026a3b557d58359e718b5
Tidiane <atdia97@gmail.com>
2021-08-18 14:34:41 +0200

    Add urls tests

---
+++ tests/wagtail_live/test_urls.py
@@ -0,0 +1,30 @@
+from django.test import override_settings
+
+from tests.utils import reload_urlconf
+from wagtail_live.exceptions import WebhookSetupError
+from wagtail_live.receivers.telegram import TelegramWebhookReceiver
+
+
+@override_settings(WAGTAIL_LIVE_PUBLISHER="")
+def test_urls_no_publisher(caplog):
+    reload_urlconf()
+
+    assert (
+        caplog.messages[0]
+        == "You haven't specified a publisher class in your settings."
+    )
+
+
+@override_settings(
+    WAGTAIL_LIVE_RECEIVER="wagtail_live.receivers.telegram.TelegramWebhookReceiver"
+)
+def test_urls_webhook_setup_error(caplog, mocker):
+    mocker.patch.object(
+        TelegramWebhookReceiver, "webhook_connection_set", return_value=False
+    )
+    mocker.patch.object(
+        TelegramWebhookReceiver, "set_webhook", side_effect=WebhookSetupError
+    )
+    reload_urlconf()
+
+    assert caplog.messages[0] == "Webhook setup failed"
