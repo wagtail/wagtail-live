1df7231682ec21eb68bd3c9110240166c50367ba
Tidiane Dia <atdia97@gmail.com>
2021-10-18 16:33:49 +0200

    Update tests for channels views

--- tests/wagtail_live/webapp/test_channels.py
+++ tests/wagtail_live/webapp/test_channels.py
@@ -1,5 +1,7 @@
 """ Webapp Channel test suite """
 
+from django.contrib.auth.models import User
+from django.core.exceptions import ImproperlyConfigured
 from django.test import TestCase
 
 from wagtail_live.webapp.models import Channel
@@ -15,30 +17,26 @@ class ChannelTestCaseSetUp(TestCase):
             Channel.objects.create(
                 channel_name=f"channel_{i}",
             )
+        cls.user = User.objects.create(username="Tester")
+
+    def setUp(self):
+        self.client.force_login(self.user)
 
 
 class ChannelViewsTests(ChannelTestCaseSetUp):
     def test_channels_listing_status_code(self):
-        """Response is 200 OK."""
-
         response = self.client.get("/webapp/channels/")
         self.assertEqual(response.status_code, 200)
 
     def test_channels_listing_count(self):
-        """Rendered context contains channels_count channels."""
-
         response = self.client.get("/webapp/channels/")
         self.assertEqual(len(response.context["channels"]), self.channels_count)
 
     def test_channel_listing_status_code(self):
-        """Response is 200 OK."""
-
         response = self.client.get("/webapp/channels/channel_3/")
         self.assertEqual(response.status_code, 200)
 
     def test_retrieve_channel(self):
-        """Response contains expected channel."""
-
         response = self.client.get("/webapp/channels/channel_1/")
         channel_exp = Channel.objects.get(channel_name="channel_1")
         channel_got = response.context["channel"]
@@ -46,8 +44,6 @@ class ChannelViewsTests(ChannelTestCaseSetUp):
         self.assertEqual(channel_exp, channel_got)
 
     def test_retrieve_non_existent_channel(self):
-        """Response is 404 Not Found."""
-
         response = self.client.get("/webapp/channels/non_existent/")
         self.assertEqual(response.status_code, 404)
 
@@ -71,58 +67,40 @@ class ChannelAPITests(ChannelTestCaseSetUp):
         return response
 
     def test_queryset_order_is_reversed(self):
-        """Queryset order is reversed."""
-
         first_channel = Channel.objects.get(channel_name="channel_1")
         self.assertEqual(Channel.objects.last(), first_channel)
 
     def test_create_channel_status_code(self):
-        """Response is 201 CREATED."""
-
         response = self.create_channel(channel_name="channel_6")
         self.assertEqual(response.status_code, 201)
 
     def test_create_channel_returns_new_channel_name(self):
-        """Response contains new channel name."""
-
         response = self.create_channel(channel_name="new_channel_name")
         self.assertEqual(response.json()["channel_name"], "new_channel_name")
 
     def test_channels_count_after_channel_creation(self):
-        """Channels count has increased by 1."""
-
         self.create_channel(channel_name="new_channel")
         self.assertEqual(Channel.objects.count(), self.channels_count + 1)
 
     def test_query_created_channel(self):
-        """Created channel is stored."""
-
         self.create_channel(channel_name="new_channel")
         new_channel = Channel.objects.get(channel_name="new_channel")
         self.assertEqual(new_channel.channel_name, "new_channel")
 
     def test_create_channel_with_already_existing_channel(self):
-        """New channel isn't created. 400 Bad Request"""
-
         response = self.create_channel("channel_1")
         self.assertEqual(response.status_code, 400)
         self.assertEqual(Channel.objects.count(), self.channels_count)
 
     def test_delete_channel_status_code(self):
-        """Response is 204 DELETED."""
-
         response = self.delete_channel(channel_name="channel_4")
         self.assertEqual(response.status_code, 204)
 
     def test_channels_count_after_channel_delete(self):
-        """Channels count has decreased by 1."""
-
         self.delete_channel(channel_name="channel_2")
         self.assertEqual(Channel.objects.count(), self.channels_count - 1)
 
     def test_query_deleted_channel(self):
-        """Deleted channel does no longer exist."""
-
         channel_to_delete = "channel_1"
         msg = "Channel matching query does not exist"
         with self.assertRaisesMessage(Channel.DoesNotExist, msg):
@@ -130,8 +108,28 @@ class ChannelAPITests(ChannelTestCaseSetUp):
             Channel.objects.get(channel_name=channel_to_delete)
 
     def test_delete_non_existent_channel(self):
-        """No channel is deleted. 404 Not Found"""
-
         response = self.delete_channel(channel_name="non_existent_channel")
         self.assertEqual(response.status_code, 404)
         self.assertEqual(Channel.objects.count(), self.channels_count)
+
+
+class AuthenticationTests(TestCase):
+    def test_missing_webapp_login_url_setting(self):
+        expected = (
+            "You haven't specified the WEBAPP_LOGIN_URL in your settings. "
+            "It is required if you intend to use the webapp interface."
+        )
+        with self.assertRaisesMessage(ImproperlyConfigured, expected):
+            self.client.get("/webapp/channels/")
+
+    def test_login_required_for_channels_views(self):
+        webapp_login_url = "/login/"
+
+        with self.settings(WEBAPP_LOGIN_URL=webapp_login_url):
+            response = self.client.get("/webapp/channels/")
+            self.assertEqual(response.url, f"{webapp_login_url}?next=/webapp/channels/")
+
+            response = self.client.get("/webapp/channels/channel_2/")
+            self.assertEqual(
+                response.url, f"{webapp_login_url}?next=/webapp/channels/channel_2/"
+            )
